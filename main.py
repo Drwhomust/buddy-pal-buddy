538c1650a761ef6290cc48c205144d38ef8a1216
Drwhomust <rabbids8008@gmail.com>
2026-06-01 19:42:48 -0700

    just starting

--- main.py
+++ main.py
@@ -1,2 +1,21 @@
 from sense_hat import SenseHat
+from sense_emu import SenseEmu
 from time import sleep
+
+# FLAGS!!
+# Feel free to change this however you want! >~<
+
+use_emu_sense = False # this uses the emu sense app for the pi
+
+
+sense = SenseHat()
+
+if use_emu_sense == True:
+    sense = SenseEmu
+
+# the preload
+sense.clear()
+sense.show_message("Now loading....")
+
+# the real load
+
