e993d04cb49fc63939c6588dfb5901f1ada2c217
Drwhomust <rabbids8008@gmail.com>
2026-06-02 00:34:21 -0700

    Update main.py
    I made SO much progress and i will keep going. I will post a dev log on the stardance soon...

--- main.py
+++ main.py
@@ -8,11 +8,13 @@ import atexit
 # Feel free to change this however you want! >~<
 
 use_emu_sense = False # this uses the emulator sense app for the pi
-use_Celelis = False # For people who live outside the US
-use_Low_Power_Mode = True # this dims the light to use less power
+use_Low_Power_Mode = False # this dims the light to use less power
 round_up_temp = True # rounds up the temp value to the nearest number (no deicamls)
+show_Hints = True # I will tell you what I want though the debug logs
 
 # -------------------------------------------------------------------
+
+# the rest is code:
 
 if use_emu_sense:
     from sense_emu import SenseHat
@@ -33,6 +35,11 @@ sense.show_message("Now loading....")
 
 # the real load
 
+emotion = "Happy"
+# Buddy can have differnt emotions
+# Happy, Hungry, Cold, Hot
+# all do what they do from their name
+
 # functions that might be needed!
 
 def CelelisToFahrenheit(input):
@@ -46,22 +53,23 @@ def exit_Clean_Up():
     sense.show_message("Goodbye! :)")
     sense.clear()
 
-# variables!
+def temp_Check(temputer):
+    if temputer >= 95:
+        emotion = "Hot"
+        if show_Hints:
+            print("I am way too hot! Cool me down!")
 
-emotion = "Happy"
-# Buddy can have differnt emotions
-# Happy, Hungry, Cold, Hot, waterly
-# all do what they do from their name
+    if temputer <= 68:
+        emotion = "Cold"
+        if show_Hints:
+            print("I am too cold! Warm me up!")
+
+# variables!
 
 hunger = 100 # how hungery they get
 
 temp = CelelisToFahrenheit(sense.get_temperature()) # get's init temp
 
-if temp >= 95:
-    emotion = "Hot"
-else:
-    if temp <= 68:
-        emotion = "Cold"
 
 print(temp)
 
@@ -72,6 +80,8 @@ atexit.register(exit_Clean_Up)
 X = [255,255,255] # White
 O = [42,161,252] # The Blue
 C = [42,88,252] # the eyes and mouth
+R = [255,0,0] # red for heat
+B = [116,184,252] # cold sweats
 
 # Faces!
 
@@ -86,3 +96,17 @@ happy_face = [
     X, X, O, C, C, C, O, X,
 ]
 
+blink = [
+    X, X, X, X, X, X, X, X,
+    X, X, O, O, O, O, O, X,
+    X, O, X, X, X, X, X, O,
+    X, O, X, X, X, X, X, O,
+    X, O, C, C, C, C, C, O,
+    X, O, X, X, X, X, X, O,
+    X, O, C, X, X, X, C, O,
+    X, X, O, C, C, C, O, X,
+]
+
+while True:
+    sense.set_pixels(happy_face)
+    sleep(1)
